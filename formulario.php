ec3199abb893d7e776d13db7bf8d5afed65b6338
SaletD <108552042+SaletD@users.noreply.github.com>
2023-09-08 21:44:05 -0500

    Update formulario.php

--- formulario.php
+++ formulario.php
@@ -41,3 +41,45 @@
         </div>
     </body>
 </html> https://www.youtube.com/watch?app=desktop&v=fGHMJynoJpc  
+<!DOCTYPE html>
+<html lang="en">
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE-edge">
+        <meta name="viewport"content="width=device-width, initial-scale1.0">
+        <title>Modificar y Eliminar </title>
+    </head>
+    <body>
+        <div>
+            <form action="buscar.php" method ="post">
+                <input type ="text" name="buscar" id="">
+                <input type ="submit" value="Buscar">
+                <a href="nuevo.php">Nuevo</a>
+            </form>
+        </div>
+        <div>
+            <table>
+                <tr>
+                    <td>Nombre libro:</td>
+                    <td>Autor:</td>
+                    <td>ISBN:</td>
+                    <td>Descripción:</td>
+                </tr>
+                <?php
+                $cnx=mysqli_connect("localhost","root","root123456","dbformulario");
+                $sql="SELECT nom, au, isbn, descri FROM tablalibro by id desc";
+                $rta=mysqli_query($cnx,$sql);
+                while ($mostrar=mysqli_fetch_row($rta)){
+                ?>
+                <tr>
+                    <td><?php echo $mostrar['0']?></td>
+                    <td><?php echo $mostrar['1']?></td>
+                    <td><?php echo $mostrar['2']?></td>
+                </tr>
+                <?php
+                }
+                ?>
+            </table>
+        </div>
+    </body>
